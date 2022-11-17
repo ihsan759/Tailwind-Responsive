63e6c0ee60cda46eb872c487903003156eaadba1
Ichsan <mihsandf759@gmail.com>
2022-11-17 09:11:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    fontFamily: {
+      body: ["Inter"],
+      sans: ["Inter"],
+      serif: ["Inter"],
+      mono: ["Inter"],
+    },
+    container: {
+      padding: "20px",
+      center: true,
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1170px",
+        "2xl": "1536px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
